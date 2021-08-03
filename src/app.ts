0d4913528c359f314fa8fd73d58e4a8be8c63028
Paul <paulmakles@gmail.com>
2021-08-03 21:21:50 +0100

    Fix version.

--- src/app.ts
+++ src/app.ts
@@ -28,7 +28,7 @@ let config = new Config();
 ipcRenderer.on('config', (_, data) => config.apply(data));
 
 contextBridge.exposeInMainWorld("isNative", true);
-contextBridge.exposeInMainWorld("nativeVersion", "1.0.0");
+contextBridge.exposeInMainWorld("nativeVersion", "1.0.1");
 contextBridge.exposeInMainWorld(
     "native", {
         min: () => ipcRenderer.send('min'),
