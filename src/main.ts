b07318fa21d5c6f2ab841aacef858716018bf9c2
Paul Makles <paulmakles@gmail.com>
2022-05-10 12:13:02 +0100

    feat: add background colour to window
    closes #14

--- src/main.ts
+++ src/main.ts
@@ -63,6 +63,8 @@ function createWindow() {
         width: mainWindowState.width,
         height: mainWindowState.height,
 
+        backgroundColor: "#191919",
+
         minWidth: 300,
         minHeight: 300,
     });
