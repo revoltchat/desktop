6b931ea0545d85cb17defc18d77fc272e453153b
Frolleks <spaceactive.akbar@gmail.com>
2022-06-11 19:42:26 +0700

    fix: fix icon not loading on production builds

--- src/main.ts
+++ src/main.ts
@@ -57,7 +57,7 @@ function createWindow() {
     mainWindow = new BrowserWindow({
         autoHideMenuBar: true,
         title: "Revolt",
-        icon: "../build/icons/icon.png",
+        icon: path.join(__dirname, "..", "build", "icons", "icon.png"),
 
         frame: initialConfig.frame,
 
